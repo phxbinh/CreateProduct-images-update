d2d39a8037e3e4e8008af0c94f3f1c7731332366
phxbinh <phxbinh@gmail.com>
2026-01-24 21:35:48 +0700

    Update upload-product-thumbnail.js

--- api/products/upload-product-thumbnail.js
+++ api/products/upload-product-thumbnail.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
+export const runtime = 'edge';
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
